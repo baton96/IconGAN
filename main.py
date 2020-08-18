c035817aef35e3ccd22c3553c77e8e7d385fc0c1
baton96 <podolce0@gmail.com>
2020-08-18 16:04:27 +0200

    Grayscaling

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from bs4 import BeautifulSoup
-from requests import get
-import pandas as pd
-import numpy as np
-import imagehash
-import cv2
 import os
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+import cv2
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+from requests import get
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/50.0.2661.102 Safari/537.36'}
 nPages = 1
+
 
 def fetchMetadataFreepik():
     if not os.path.exists('freepik'):
@@ -16,7 +18,7 @@ def fetchMetadataFreepik():
     metadata = open('freepik/metadata.csv', 'w')
     metadata.write('URL,Likes,Downloads,Keyword,Title\n')
 
-    for i in range(1, nPages+1):
+    for i in range(1, nPages + 1):
         page = get(f'https://www.freepik.com/search?type=icon&page={i}', headers=headers)
         soup = BeautifulSoup(page.content, 'html.parser')
         figures = soup.find_all('figure')
@@ -26,9 +28,10 @@ def fetchMetadataFreepik():
             downloads = figure['data-downloads']
             keyword = figure['data-first-keyword']
             title = figure.find('p', class_='title').text
-    
+
             metadata.write(f'{image},{likes},{downloads},{keyword},{title}\n')
     metadata.close()
+
 
 def fetchImgsFreepik():
     if not os.path.exists('freepik/img'):
@@ -37,6 +40,8 @@ def fetchImgsFreepik():
     for url in metadata['URL']:
         with open(f'freepik/img/{url}', 'wb') as f:
             f.write(get(f'https://image.freepik.com/free-icon/{url}').content)
+
+
 '''
 def flipImgs():
     if not os.path.exists('flipped'):
@@ -46,7 +51,9 @@ def flipImgs():
         cv2.imwrite(f'flipped/flipped{filename}', cv2.flip(original, 1))
 '''
 
+
 def processImgsFreepik():
+    #imgs = []
     if not os.path.exists('freepik/processed'):
         os.makedirs('freepik/processed')
     for filename in os.listdir('freepik/img'):
@@ -55,6 +62,10 @@ def processImgsFreepik():
         # Padding
         h_original, w_original, _ = original.shape
         h_desired, w_desired, = 626, 626
+        if (h_original, w_original) == (h_desired, w_desired):
+            cv2.imwrite(f'freepik/processed/{filename}', original)
+            continue
+
         white = (255, 255, 255)
         padded = np.full((h_desired, w_desired, 3), white, dtype=np.uint8)
         x = (w_desired - w_original) // 2
@@ -64,13 +75,19 @@ def processImgsFreepik():
         # Grayscaling
         gray = cv2.cvtColor(padded, cv2.COLOR_BGR2GRAY)
 
+        # Quality should be 0 or 100?
+        cv2.imwrite(f'freepik/processed/{filename}', gray, [int(cv2.IMWRITE_JPEG_QUALITY ), 100])
+
         # Most of pixels are either 0 or 255 so
         # normalize them from [0, 255] to [-1, 1]
         gray = gray / 255.0
         gray -= 0.5
         gray *= 2
-        gray.dtype = np.float16
+        gray = gray.astype('float32')
         np.save(f'freepik/processed/{filename}', gray)
+        # imgs += [gray]
+
+    # np.savez_compressed('freepik/processedFile', *imgs)
 
 
 def fetchMetadataFlaticon():
@@ -78,33 +95,29 @@ def fetchMetadataFlaticon():
         os.makedirs('flaticon')
 
     metadata = open('flaticon/metadata.csv', 'w')
-    metadata.write('URL,Downloads\n')
+    metadata.write('URL,Downloads,Name,Keyword,Category_id,Category_name\n')
 
     for i in range(1, nPages + 1):
-        page = get(f'https://www.flaticon.com/search/{i}?search-type=icons&license=selection', headers=headers)
+        page = get(
+            f'https://www.flaticon.com/search/{i}?search-type=icons&license=selection&color=1&stroke=1',
+            #f'https://www.flaticon.com/search/{i}?search-type=icons&license=selection&color=1&stroke=2',
+            headers=headers)
         soup = BeautifulSoup(page.content, 'html.parser')
         icons = soup.find_all('li', class_='icon')
         for icon in icons:
             try:
                 img = '/'.join(icon['data-icon_src'].split('/')[5:])
-                img = img.split('.')[0]+'.png'
+                img = img.split('.')[0] + '.png'
                 downloads = icon['data-downloads']
-                metadata.write(f'{img},{downloads}\n')
+                name = icon['data-name']
+                keyword = icon['data-keyword']
+                category_id = icon['data-category_id']
+                category_name = icon['data-category_name']
+
+                metadata.write(f'{img},{downloads},{name},{keyword},{category_id},{category_name}\n')
             except:
                 break
-            '''
-            name = icon['data-name']
-            color = icon['data-color'] # 1-black&white, 2-color
-            #style = icon['data-style']            
-            category_id = icon['data-category_id']
-            category_name = icon['data-category_name']
-            family_id = icon['data-family_id']
-            family_name = icon['data-family_name']
-            family_name = icon['data-family_name']
-            keyword = icon['data-keyword']
-            '''
     metadata.close()
-
 
 def fetchImgsFlaticon():
     if not os.path.exists('flaticon/img'):
@@ -121,6 +134,8 @@ def processImgsFlaticon():
     for filename in os.listdir('flaticon/img'):
         original = cv2.imread(f'flaticon/img/{filename}', cv2.IMREAD_UNCHANGED)
 
+        # gray = 255 - original[:, :, 3]
+        # cv2.imwrite(f'flaticon/processed/{filename}', gray)
         # White instead of transparent background
         mask = original[:, :, 3] == 0
         original[mask] = [255, 255, 255, 255]
@@ -133,5 +148,5 @@ def processImgsFlaticon():
         gray = gray / 255.0
         gray -= 0.5
         gray *= 2
-        gray.dtype = np.float16
+        gray = gray.astype('float32')
         np.save(f'flaticon/processed/{filename}', gray)
